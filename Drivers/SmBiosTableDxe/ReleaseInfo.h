24fc39891a44a65f69358b62386a4608e7e96046
Bingxing Wang <i@imbushuo.net>
2019-03-07 01:43:26 -0500

    SMBIOS: Fix re-def

--- Drivers/SmBiosTableDxe/ReleaseInfo.h
+++ Drivers/SmBiosTableDxe/ReleaseInfo.h
@@ -1,11 +1,54 @@
-#ifndef __SMBIOS_RELEASE_INFO_H__
-#define __SMBIOS_RELEASE_INFO_H__
+/** @file
+  SMBIOS Table for Qualcomm ARM platform
+  Derived from EmulatorPkg package
+
+  Note SMBIOS 2.7.1 Required structures:
+    BIOS Information (Type 0)
+    System Information (Type 1)
+    Board Information (Type 2)
+    System Enclosure (Type 3)
+    Processor Information (Type 4) - CPU Driver
+    Cache Information (Type 7) - For cache that is external to processor
+    System Slots (Type 9) - If system has slots
+    Physical Memory Array (Type 16)
+    Memory Device (Type 17) - For each socketed system-memory Device
+    Memory Array Mapped Address (Type 19) - One per contiguous block per Physical Memroy Array
+    System Boot Information (Type 32)
+
+
+  Copyright (c), 2017, Andrey Warkentin <andrey.warkentin@gmail.com>
+  Copyright (c), 2018-2019, Bingxing Wang <uefi-oss-projects@imbushuo.net>
+  Copyright (c), Microsoft Corporation. All rights reserved.
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+
+  Copyright (c) 2012, Apple Inc. All rights reserved.<BR>
+  Copyright (c) 2013 Linaro.org
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef __SMBIOS_RELEASE_INFO_PREGEN_H__
+#define __SMBIOS_RELEASE_INFO_PREGEN_H__
 #ifdef __IMPL_COMMIT_ID__
 #undef __IMPL_COMMIT_ID__
 #endif
-#define __IMPL_COMMIT_ID__ "a413f665"
+#define __IMPL_COMMIT_ID__ "00000000"
 #ifdef __RELEASE_DATE__
 #undef __RELEASE_DATE__
 #endif
-#define __RELEASE_DATE__ "12/28/2018"
+#define __RELEASE_DATE__ "01/01/1970"
 #endif
